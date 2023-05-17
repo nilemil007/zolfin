8bc392d6614660fa301b49438a813f67db5807bf
Emil Sadekin Islam <nilemil007@gmail.com>
2023-05-17 23:23:06 +0600

    index.php file modified

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -6,15 +6,15 @@
     <title>{{ !isset($title) ? config('app.name') : $title .' | '. config('app.name') }}</title>
 
     <!-- favicon -->
-    <link rel="icon" href="{{ asset('public/favicon.ico') }}" sizes="16x16" type="image/ico"/>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="16x16" type="image/ico"/>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="{{ asset('public/backend/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{ asset('public/backend/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('public/backend/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/dist/css/adminlte.min.css') }}">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">
 <div class="wrapper">
@@ -67,13 +67,13 @@
 
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
-<script src="{{ asset('public/backend/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap -->
-<script src="{{ asset('public/backend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="{{ asset('public/backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('public/backend/dist/js/adminlte.js') }}"></script>
+<script src="{{ asset('backend/dist/js/adminlte.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
 {{--<script src="{{ asset('public/backend/dist/js/demo.js') }}"></script>--}}
 
--- resources/views/backend/guest-layout.blade.php
+++ resources/views/backend/guest-layout.blade.php
@@ -6,25 +6,25 @@
     <title>{{ !isset($title) ? config('app.name') : $title .' | '. config('app.name') }}</title>
 
     <!-- favicon -->
-    <link rel="icon" href="{{ asset('public/favicon.ico') }}" sizes="16x16" type="image/ico"/>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="16x16" type="image/ico"/>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('public/backend/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ asset('public/backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('public/backend/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/dist/css/adminlte.min.css') }}">
 </head>
 <body class="hold-transition login-page">
 
 {{ $slot }}
 
 <!-- jQuery -->
-<script src="{{ asset('public/backend/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('public/backend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('public/backend/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('backend/dist/js/adminlte.min.js') }}"></script>
 </body>
 </html>
