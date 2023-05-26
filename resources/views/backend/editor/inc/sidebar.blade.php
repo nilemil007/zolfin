8fc5eb336ec14a3ffe1520dcf9ffb4977e55aa7f
Emil Sadekin Islam <nilemil007@gmail.com>
2023-05-26 16:51:29 +0600

    Modify sidebar code

--- resources/views/backend/editor/inc/sidebar.blade.php
+++ resources/views/backend/editor/inc/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('home') }}" class="brand-link">
-        <img src="{{ asset('public/zolfin/assets/img/logo-6.png') }}" alt="Dashboard Logo" class="brand-image">
+        <img src="{{ asset('zolfin/assets/img/logo-6.png') }}" alt="Dashboard Logo" class="brand-image">
     </a>
 
     <!-- Sidebar -->
@@ -9,7 +9,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('public/backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{ auth()->user()->name }}</a>
